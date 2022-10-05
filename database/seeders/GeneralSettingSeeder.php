4d0e6b372ea55d3d5ba5ebd92aac3dd52ff9a89b
Chan <aun@smartblock.pro>
2022-10-05 17:02:30 +0800

    add general setting seeder file

---
+++ database/seeders/GeneralSettingSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SysGeneral;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class GeneralSettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        SysGeneral::insert([
+            'country_id' => 1,
+            'type' => 'gender',
+            'code' => 'male',
+            'name' => 'Male',
+            'seq_no' => 0
+        ], [
+            'country_id' => 1,
+            'type' => 'gender',
+            'code' => 'female',
+            'name' => 'Female',
+            'seq_no' => 10
+        ]);
+    }
+}
